eb879fd1d0e1450cc3d2e71438bb32f320183b49
Danilo Becke <danilobecke@gmail.com>
2023-11-09 06:50:37 -0300

    Customize LinkItem font variant

--- src/components/elements/LinkItem/LinkItem.tsx
+++ src/components/elements/LinkItem/LinkItem.tsx
@@ -1,10 +1,11 @@
 import { HTMLAttributeAnchorTarget } from "react";
 
 import { Link, Typography } from "@mui/material";
+import { Variant } from "@mui/material/styles/createTypography";
 
-function LinkItem(props: { title: string, onClick?: () => void, href?: string, target?: HTMLAttributeAnchorTarget }) {
+function LinkItem(props: { title: string, onClick?: () => void, href?: string, target?: HTMLAttributeAnchorTarget, variant?: Variant }) {
     return (
-        <Typography variant="h5" sx={{ ['& :hover']: { cursor: 'pointer' } }}>
+        <Typography variant={props.variant ?? "h5"} sx={{ ['& :hover']: { cursor: 'pointer' } }}>
             <Link onClick={props.onClick} href={props.href} target={props.target}>{props.title}</Link>
         </Typography>
     )
